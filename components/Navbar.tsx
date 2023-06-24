2435c0e2ba07426eef443e676510c6a0a1de5411
Raj Chavda <raj.theappideas@gmail.com>
2023-06-24 19:33:51 +0530

    navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MobileMenu from "./MobileMenu";
 import NavbarItem from "./NavbarItem";
-import { BsChevronDown, BsChevronUp } from "react-icons/bs";
+import { BsChevronDown, BsChevronUp, BsSearch, BsBell } from "react-icons/bs";
+import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET = 66;
 
 const Navbar = () => {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 bg-zinc-900 bg-opacity-90 ">
+      <div
+        className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${
+          showBackground ? "bg-zinc-900 bg-opacity-90" : ""
+        }`}
+      >
+        {" "}
         <img src="/images/logo.png" className="h-4 lg:h-7" alt="logo" />
         <div className="flex-row ml-8 gap-7 lg:flex hidden">
           <NavbarItem label="Home" />
@@ -23,12 +49,34 @@ const Navbar = () => {
           className="lg:hidden transition selection:normal-case duration-300 flex flex-row items-center gap-2 cursor-pointer ml-8 relative"
         >
           <p className="text-white text-sm">Browse</p>
-          {showMobileMenu ? (
-            <BsChevronUp className="text-white transition duration-300" />
-          ) : (
-            <BsChevronDown className="text-white transition duration-300" />
-          )}
+          <BsChevronDown
+            className={`w-4 text-white fill-white transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
           <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer">
+            <BsBell />
+          </div>
+          <div
+            onClick={() => setShowAccountMenu(!showAccountMenu)}
+            className="flex flex-row selection:normal-case items-center gap-2 cursor-pointer relative"
+          >
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <img src="/images/default-blue.png" alt="" />
+            </div>
+            <BsChevronDown
+              className={`w-4 text-white fill-white transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
+          </div>
         </div>
       </div>
     </nav>
